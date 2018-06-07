b8ace3ec4abb6a05f1fd2a7bfce86d80482f54d2
Brian Le <leb1@seattleu.edu>
2018-06-07 16:08:26 -0700

    removed grid components

---
+++ Azure/model/PostcardModel.js
@@ -0,0 +1,43 @@
+"use strict";
+exports.__esModule = true;
+var Mongoose = require("mongoose");
+var DataAccess_1 = require("./../DataAccess");
+var mongooseConnection = DataAccess_1.DataAccess.mongooseConnection;
+var mongooseObj = DataAccess_1.DataAccess.mongooseInstance;
+var PostcardModel = /** @class */ (function () {
+    function PostcardModel() {
+        this.createSchema();
+        this.createModel();
+    }
+    PostcardModel.prototype.createSchema = function () {
+        this.schema = new Mongoose.Schema({
+            postcardID: Number,
+            userID: Number,
+            title: String,
+            imageURL: String,
+            description: String,
+            rating: Number,
+            cost: Number,
+            activityLocation: String,
+            activityCity: String
+        }, { collection: 'postcards' });
+    };
+    PostcardModel.prototype.createModel = function () {
+        this.model = mongooseConnection.model("Postcards", this.schema);
+    };
+    PostcardModel.prototype.retrieveAllPostcards = function (response) {
+        var query = this.model.find({});
+        query.exec(function (err, itemArray) {
+            console.log(itemArray.json);
+            response.json(itemArray);
+        });
+    };
+    PostcardModel.prototype.retrievePostcardDetails = function (response, filter) {
+        var query = this.model.findOne(filter);
+        query.exec(function (err, itemArray) {
+            response.json(itemArray);
+        });
+    };
+    return PostcardModel;
+}());
+exports.PostcardModel = PostcardModel;
